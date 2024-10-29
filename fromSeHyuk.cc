1450f3d379e24513e9c4f258a0a234bb23f329af
smsterry <smsterry@kaist.ac.kr>
2024-10-29 18:34:45 +0900

    Hmm.. why he did change the message?

--- fromSeHyuk.cc
+++ fromSeHyuk.cc
@@ -1,8 +1,9 @@
 #include <iostream>
 
 int main(void) {
-    
+
     std::cout << "Hello this is SeHyuk" << std::endl;
+    std::cout << "He suddenly changed the message" << std::endl;
 
     return 0;
 }
