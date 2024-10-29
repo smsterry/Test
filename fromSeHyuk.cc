a3e446cd486f132f2aac2551845b2c9af6c754ea
smsterry <smsterry@kaist.ac.kr>
2024-10-29 19:10:33 +0900

    why always you require msg?

--- fromSeHyuk.cc
+++ fromSeHyuk.cc
@@ -5,7 +5,7 @@ int main(void) {
     std::cout << "Hello this is SeHyuk" << std::endl;
     std::cout << "He suddenly changed the message" << std::endl;
     std::cout << "Interesting..." << std::endl;
-    std::cout << "He didn't like sub.. what a mean guy" << std::endl;
+    std::cout << "He finally became a dev. Congrats!" << std::endl;
 
     return 0;
 }
