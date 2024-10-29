2630af46f1ea8c0cc2ab6d55180672950c2eea0b
smsterry <smsterry@kaist.ac.kr>
2024-10-29 18:38:49 +0900

    wow

--- fromSeHyuk.cc
+++ fromSeHyuk.cc
@@ -4,6 +4,7 @@ int main(void) {
 
     std::cout << "Hello this is SeHyuk" << std::endl;
     std::cout << "He suddenly changed the message" << std::endl;
+    std::cout << "Interesting..." << std::endl;
 
     return 0;
 }
