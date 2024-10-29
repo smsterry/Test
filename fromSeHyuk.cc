24b525a2f121fe3636c89ea544b5b25f159a2dbb
smsterry <smsterry@kaist.ac.kr>
2024-10-29 19:04:13 +0900

    new branch test

--- fromSeHyuk.cc
+++ fromSeHyuk.cc
@@ -5,6 +5,7 @@ int main(void) {
     std::cout << "Hello this is SeHyuk" << std::endl;
     std::cout << "He suddenly changed the message" << std::endl;
     std::cout << "Interesting..." << std::endl;
+    std::cout << "He didn't like sub.. what a mean guy" << std::endl;
 
     return 0;
 }
